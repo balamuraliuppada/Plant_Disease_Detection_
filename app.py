8c20871d873948b1641bf410cb52a4fa98534632
balamuraliuppada <balubalamurali071@gmail.com>
2026-02-21 22:00:24 +0530

    keras fixed

--- app.py
+++ app.py
@@ -202,8 +202,8 @@ def load_models():
     models = {"Rice": None, "Pulses": None}
 
     model_paths = {
-        "Rice": "models/rice_model.keras",
-        "Pulses": "models/pulses_model.keras"
+        "Rice": "models/rice_model_fixed.keras",
+        "Pulses": "models/pulses_model_fixed.keras"
     }
 
     for name, path in model_paths.items():
