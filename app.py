99af9542250a8cc67366940ff8fd9bf4dc003428
balamuraliuppada <balubalamurali071@gmail.com>
2026-02-21 22:02:29 +0530

    rice model fixed

--- app.py
+++ app.py
@@ -202,7 +202,7 @@ def load_models():
     models = {"Rice": None, "Pulses": None}
 
     model_paths = {
-        "Rice": "models/rice_model_fixed.keras",
+        "Rice": "models/rice_model_clean.keras",
         "Pulses": "models/pulses_model_fixed.keras"
     }
 
